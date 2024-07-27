ef2c68f61f603c5e1b9ce1f071309403e90943c7
GityBoyy <153344018+GityBoyy@users.noreply.github.com>
2024-07-27 16:23:14 +0530

    FIXING

--- src/main/java/net/cebularz/morewolfs/mixin/MoreWolfVarsMixin.java
+++ src/main/java/net/cebularz/morewolfs/mixin/MoreWolfVarsMixin.java
@@ -7,6 +7,8 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.animal.WolfVariant;
@@ -17,6 +19,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Random;
 
